d2ab537cab9940503af38fdb61ea9ef219b044d4
yj0524 <jeonyunjae_kr@naver.com>
2023-03-04 13:24:02 +0900

    1.0.2

--- src/main/java/com/yj0524/Main.java
+++ src/main/java/com/yj0524/Main.java
@@ -1,7 +1,6 @@
 package com.yj0524;
 
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
